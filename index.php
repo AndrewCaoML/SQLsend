13234fe0cb703419fdbbb2be3b7cfae06bd62181
Andrew <andrew.cao.mn@gmail.com>
2019-04-12 22:59:43 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML!>
+<html>
+    <head>
+    	<title>SQL Send</title>
+    </head>
+    <body>
+    	<form action="action_page.php" method="post">
+  		Input 1: <input type="text" name="1"><br>
+  		Input 2: <input type="text" name="2"><br>
+  		Input 3: <input type="text" name="3"><br>
+  		<input type="submit" value="Submit">
+	</form>
+    </body>
+</html>
